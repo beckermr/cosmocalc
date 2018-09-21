aa7d7cb58f05a36d446e02b45a9117d93eb16556
Matthew R. Becker <beckermr@users.noreply.github.com>
2018-09-20 19:35:05 -0500

    penance (#2)

--- setup.py
+++ setup.py
@@ -1,30 +1,24 @@
-#!/usr/bin/env python
-
-"""
-setup.py file for SWIG wrapping of cosmocalc
-"""
-
-import os
-from distutils.core import setup, Extension
+from setuptools import setup, Extension, find_packages
 import glob
 
-csrc = glob.glob("*.c")
+csrc = glob.glob("src/*.c")
 srcs = []
 for src in csrc:
-    if src != "main.c" and src != "fftlog.c" and src != "test_code.c":
+    if src not in ["src/main.c", "test_code.c"]:
         srcs.append(src)
+srcs += ['cosmocalc/cosmocalc_wrap.c']
+print(srcs)
+cosmocalc_module = Extension(
+    'cosmocalc._cosmocalc',
+    sources=srcs,
+    extra_link_args=["-lfftw3", "-lm", "-lgsl", "-lgslcblas"],
+)
 
-cosmocalc_module = Extension('_cosmocalc',
-                             sources=srcs,
-                             extra_compile_args = [os.path.expandvars("-I${SLAC_GSL_INC}"),"-I/opt/local/include"],
-                             extra_link_args = [os.path.expandvars("-L${SLAC_GSL_LIB}"),"-L/opt/local/lib","-lm","-lgsl","-lgslcblas"],
-                             )
-
-setup (name = 'cosmocalc',
-       version = '0.2',
-       author      = "Matthew R. Becker",
-       description = """cosmocalc wrapped by SWIG""",
-       ext_modules = [cosmocalc_module],
-       py_modules = ["cosmocalc"],
-       )
-
+setup(
+    name='cosmocalc',
+    version='0.2.1',
+    author="Matthew R. Becker",
+    description="cosmocalc wrapped by SWIG",
+    ext_modules=[cosmocalc_module],
+    packages=find_packages(),
+)
